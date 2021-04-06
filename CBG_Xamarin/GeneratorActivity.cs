97b478e81a224e9323956be7fa144a57e2783980
unknown <ksfff5@mst.edu>
2021-04-06 18:08:15 -0500

    Fixed a bug that prevented the Variance input from working on some devices.

--- CBG_Xamarin/GeneratorActivity.cs
+++ CBG_Xamarin/GeneratorActivity.cs
@@ -134,14 +134,14 @@ namespace CBG_Xamarin
                 Console.WriteLine("Wood: " + WoodBar.Progress);
 
                 SeekBar VarianceBar = FindViewById<SeekBar>(Resource.Id.VarianceBar);
-                Console.WriteLine("Variance: " + VarianceBar.Progress);
+                Console.WriteLine("Variance: " + VarianceBar.Progress + 3);
 
                 //Create an intent to launch the Viewer Activity
                 Intent viewerIntent = new Intent(this, typeof(ViewerActivity));
 
                 //Add the necessary data to the intent
                 viewerIntent.PutExtra("NewBoard", true);
-                viewerIntent.PutExtra("Variance", VarianceBar.Progress);
+                viewerIntent.PutExtra("Variance", VarianceBar.Progress + 3);
 
                 //Start the activity
                 StartActivity(viewerIntent);

ef9b5a72426df39c997d9df58c946466faa1ae67
unknown <ksfff5@mst.edu>
2021-04-13 17:47:35 -0500

    Added a new activity and layout for the save/load screen.

---
+++ CBG_Xamarin/SaveLoadActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CBG_Xamarin
+{
+    [Activity(Label = "SaveLoadActivity")]
+    public class SaveLoadActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_saveload);
+            
+
+        }
+    }
+}
